18f5ba69be9c493bad88db477ff0f8258b6a1ae2
Nikita Nikonovich <nikonovichnd@gmail.com>
2024-11-14 17:14:41 +0300

    chore: improve navigation menu

--- lib/src/navigation/cv_app_bar.dart
+++ lib/src/navigation/cv_app_bar.dart
@@ -12,30 +12,35 @@ class CvAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => const Size(double.infinity, 100);
 
+  static const _padding = EdgeInsets.only(top: 60);
+
   @override
   Widget build(BuildContext context) {
     final goRouterState = GoRouterStateProvider.of(context).state;
 
-    return Row(
-      children: [
-        // TODO: do something with text
-        SvAppTextButton.menu(
-          onPressed: () => context.go(RouterPath.welcome.path),
-          isActive: goRouterState.fullPath == RouterPath.welcome.path,
-          child: const Text('Welcome'),
-        ),
-        const Spacer(),
-        SvAppTextButton.menu(
-          onPressed: () => context.go(RouterPath.skills.path),
-          isActive: goRouterState.fullPath == RouterPath.skills.path,
-          child: const Text('Skills'),
-        ),
-        SvAppTextButton.menu(
-          onPressed: () => context.go(RouterPath.experience.path),
-          isActive: goRouterState.fullPath == RouterPath.experience.path,
-          child: const Text('Experience'),
-        ),
-      ],
+    return Padding(
+      padding: _padding,
+      child: Row(
+        children: [
+          // TODO: do something with text
+          SvAppTextButton.menu(
+            onPressed: () => context.go(RouterPath.welcome.path),
+            isActive: goRouterState.fullPath == RouterPath.welcome.path,
+            child: const Text('Welcome'),
+          ),
+          const Spacer(),
+          SvAppTextButton.menu(
+            onPressed: () => context.go(RouterPath.skills.path),
+            isActive: goRouterState.fullPath == RouterPath.skills.path,
+            child: const Text('Skills'),
+          ),
+          SvAppTextButton.menu(
+            onPressed: () => context.go(RouterPath.experience.path),
+            isActive: goRouterState.fullPath == RouterPath.experience.path,
+            child: const Text('Experience'),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/src/navigation/navigation.dart
+++ lib/src/navigation/navigation.dart
@@ -5,6 +5,7 @@ import 'package:go_router/go_router.dart';
 import 'paths.dart';
 import 'cv_app_bar.dart';
 import 'state_provider.dart';
+import '../ui_kit/atoms/colors.dart';
 import '../features/features.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
@@ -56,11 +57,22 @@ class ScaffoldCvApp extends StatelessWidget {
     required this.child,
   });
 
+  static const _appConstraints = BoxConstraints(maxWidth: 1366.0);
+  static const _appPadding = EdgeInsets.symmetric(horizontal: 40.0);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const CvAppBar(),
-      body: child,
+    return Container(
+      alignment: Alignment.center,
+      color: CvAppBasicColors.gloomy,
+      padding: _appPadding,
+      child: ConstrainedBox(
+        constraints: _appConstraints,
+        child: Scaffold(
+          appBar: const CvAppBar(),
+          body: child,
+        ),
+      ),
     );
   }
 }
