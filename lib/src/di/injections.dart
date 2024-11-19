5d69911ee6914a1c9c58e6cfc9ef3b9fc7c843f9
Nikita Nikonovich <nikonovichnd@gmail.com>
2024-11-19 13:25:50 +0300

    add: shared prefernces package with manager

--- lib/src/di/injections.dart
+++ lib/src/di/injections.dart
@@ -1,9 +1,11 @@
 import 'package:get_it/get_it.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:cv_website/src/features/features.dart';
 
 import '../managers/firebase_database.dart';
+import '../managers/shared_preferences.dart';
 
 final GetIt injector = GetIt.instance;
 
@@ -12,6 +14,9 @@ class DependencyInjections {
 
   static Future<void> registerDependencies() async {
     injector.registerSingleton<FirebaseDatabase>(initializeFirebaseDatabase());
+    injector.registerSingleton<SharedPreferencesManager>(
+      SharedPreferencesManagerImpl(prefs: await SharedPreferences.getInstance()),
+    );
 
     injector.registerSingleton<WelcomeRepository>(WelcomeRepositoryImpl(remoteDB: injector()));
     injector.registerSingleton<WelcomeScreenController>(
