e3758a0553b7be78aad8a6445dee7e6320e5c34e
Nikita Nikonovich <nikonovichnd@gmail.com>
2024-12-09 12:14:47 +0300

    chore: remove hash tag from url

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'src/src.dart';
 import 'src/cv_app.dart';
@@ -8,6 +9,7 @@ import 'src/cv_app.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  usePathUrlStrategy();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await DependencyInjections.registerDependencies();
 
